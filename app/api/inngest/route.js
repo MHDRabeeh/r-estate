4e5c25745c2f601e1550620238226cf6c03e4e67
Mohammed Rabeeh <rabirabz001@gmail.com>
2025-04-14 18:39:44 +0530

    solve some issue'

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,6 +1,7 @@
 import { serve } from "inngest/next";
 import { inngest,syncUserCreation } from "@/config/inngest";
 
+
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
