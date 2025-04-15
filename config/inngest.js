496efb3c568588fa318654391e264c119a8a0184
Mohammed Rabeeh <rabirabz001@gmail.com>
2025-04-15 15:40:29 +0530

    add updation logic at authentication

--- config/inngest.js
+++ config/inngest.js
@@ -4,24 +4,40 @@ import User from "../models/User";
 export const inngest = new Inngest({ id: "estate-flow" });
 
 export const syncUserCreation = inngest.createFunction(
-    {id:"create-user-estate-flow"},
-    {event:"clerk/user.created"},
-    async({event})=>{
-        console.log("Start user sync...");
-        const {id,email_addresses,first_name,image_url,last_name} = event.data
+  { id: "create-user-estate-flow" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    console.log("Start user sync...");
+    const { id, email_addresses, first_name, image_url, last_name } =
+      event.data;
 
-        const userData = {
-            _id:id,
-            email:email_addresses[0].email_address,
-            name:first_name + " " + last_name,
-            imageUrl:image_url
-        }
-        console.log("Connecting to DB...");
-        await dbConnect()
+    const userData = {
+      _id: id,
+      email: email_addresses[0].email_address,
+      name: first_name + " " + last_name,
+      imageUrl: image_url,
+    };
+    console.log("Connecting to DB...");
+    await dbConnect();
 
-        await User.create(userData)
-        console.log("User created successfully.");
+    await User.create(userData);
+    console.log("User created successfully.");
+  }
+);
 
-
-    }
-)
+export const syncUserUpdation = inngest.createFunction(
+  { id: "update-user-estate-flow" },
+  { event: "clerk/user.updated" },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+    const userData = {
+      _id: id,
+      email: email_addresses[0].email_address,
+      name: first_name + " " + last_name,
+      imageUrl: image_url,
+    };
+    await dbConnect()
+    await User.findByIdAndUpdate(id ,userData)
+  }
+);
