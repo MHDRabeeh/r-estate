25d23f2feceb94e05154fe928dd63ef1ef578c20
Mohammed Rabeeh <rabirabz001@gmail.com>
2025-04-16 11:51:39 +0530

    add listing model

---
+++ models/Listing.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+
+const ListingSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  regularPrice: {
+    type: String,
+    required: true,
+  },
+  bathrooms: {
+    type: String,
+    required: true,
+  },
+  bedrooms: {
+    type: String,
+    required: true,
+  },
+  furninshed: {
+    type: Boolean,
+    required: true,
+  },
+  parking: {
+    type: Boolean,
+    required: true,
+  },
+  type: {
+    type: Boolean,
+    required: true,
+  },
+  imageUrls: {
+    type: Array,
+    required: true,
+  },
+  userRef: {
+    type: String,
+    required: true,
+    ref: "User",
+  },
+});
+
+const Listing = mongoose.models.Listing || mongoose.model('Listing', ListingSchema);
+export default Listing;
