8f944cf3863f7baf7cbd1887fa95dff3bbea31e0
Mohammed Rabeeh <rabirabz001@gmail.com>
2025-04-28 21:02:34 +0530

    fix the build useParams issue

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
 import { ClerkProvider } from "@clerk/nextjs";
-
+import { Suspense } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,14 +22,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Header/>
-        {children}
-      </body>
-    </html>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <Suspense fallback={null}>
+            <Header />
+          </Suspense>
+          {children}
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
